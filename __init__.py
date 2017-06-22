c5761245a9abcf54087ee6cd7226c5d7d5f00954
Grant Carroll <gcarroll2603@gmail.com>
2017-06-22 19:30:45 -0400

    Added PyCharm .idea files and started implementing exec() commands

--- __init__.py
+++ __init__.py
@@ -10,13 +10,21 @@ config.read('Config.cfg')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('I\'m here')
 
+
 @client.event
 async def on_message(message):
+    call = 'await hello(message)'
+    await exec(call)
+
+
+async def hello(message):
     if message.content.lower().startswith('!hello'):
         await client.send_message(message.channel, 'Hello World')
+
 
 client.run(config.get('DiscordConfig', 'UserToken'))
